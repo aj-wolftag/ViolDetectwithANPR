a3fedad1eba274d4918eecf64e353aeed1c2d66d
aj-wolftag <arjunalakshan56@gmail.com>
2021-02-19 05:15:15 +0530

    error handled

--- violation_detection.py
+++ violation_detection.py
@@ -141,34 +141,36 @@ while True:
                     bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
                     edged = cv2.Canny(bfilter, 30, 200) #Edge detection
                     #cv2.imshow('Noise reduction img',edged)
+                    try:
+                        keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                        contours = imutils.grab_contours(keypoints)
+                        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
-                    keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                    contours = imutils.grab_contours(keypoints)
-                    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+                        location = None
+                        for contour in contours:
+                            approx = cv2.approxPolyDP(contour, 10, True)
+                            if len(approx) == 4:
+                                location = approx
+                                break
 
-                    location = None
-                    for contour in contours:
-                        approx = cv2.approxPolyDP(contour, 10, True)
-                        if len(approx) == 4:
-                            location = approx
-                            break
+                        #print(location)
 
-                    #print(location)
+                        mask = np.zeros(gray.shape, np.uint8)
+                        new_image = cv2.drawContours(mask, [location], 0,255, -1)
+                        new_image = cv2.bitwise_and(img, img, mask=mask)
+                        #cv2.imshow('mask img',new_image)
 
-                    mask = np.zeros(gray.shape, np.uint8)
-                    new_image = cv2.drawContours(mask, [location], 0,255, -1)
-                    new_image = cv2.bitwise_and(img, img, mask=mask)
-                    #cv2.imshow('mask img',new_image)
+                        (x,y) = np.where(mask==255)
+                        (x1, y1) = (np.min(x), np.min(y))
+                        (x2, y2) = (np.max(x), np.max(y))
+                        cropped_image = gray[x1:x2+1, y1:y2+1]
+                        cv2.imshow('cropped img',cropped_image)
 
-                    (x,y) = np.where(mask==255)
-                    (x1, y1) = (np.min(x), np.min(y))
-                    (x2, y2) = (np.max(x), np.max(y))
-                    cropped_image = gray[x1:x2+1, y1:y2+1]
-                    cv2.imshow('cropped img',cropped_image)
-
-                    reader = easyocr.Reader(['en'])
-                    result = reader.readtext(cropped_image)
-                    print(result)
+                        reader = easyocr.Reader(['en'])
+                        result = reader.readtext(cropped_image)
+                        print(result)
+                    except:
+                        print("Unclear Vehicle!")
                     #ANPR END------------------------------------------------------------------------------
 
                     #recoding video
